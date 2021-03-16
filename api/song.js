14922b54f4af8531590e58da3b5109e030573cb9
YukariClaerwen <yukari.8239@gmail.com>
2021-03-16 17:24:27 +0700

    sync

--- api/song.js
+++ api/song.js
@@ -83,10 +83,9 @@ module.exports = (app) => {
         request(`http://m.nhaccuatui.com/bai-hat/${name}`, async (error, response, body) => {
             const $ = await cheerio.load(body);
             let data = {};
-            console.log(body)
             if(!error && response.statusCode == 200) {
                 let key = await $('#icon-play').attr('keyencrypt');
-                console.log(key)
+                console.log($('#icon-play'))
 
                 request(`http://m.nhaccuatui.com/ajax/get-media-info?key1=${key}&key2=&key3=&ip=14.169.121.22`, async (error, response, body) =>{
                     if(!error && response.statusCode == 200) {
