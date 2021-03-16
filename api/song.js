a699d62e1b117d00374aff829c2167ffc04fd467
YukariClaerwen <yukari.8239@gmail.com>
2021-03-16 17:38:51 +0700

    sync

--- api/song.js
+++ api/song.js
@@ -86,7 +86,7 @@ module.exports = (app) => {
             let data = {};
             if(!error && response.statusCode == 200) {
                 let key = $('#icon-play').attr('keyencrypt');
-                console.log($('#icon-play'))
+                console.log($('.cover').html())
 
                 request(`http://m.nhaccuatui.com/ajax/get-media-info?key1=${key}&key2=&key3=&ip=14.169.121.22`, async (error, response, body) =>{
                     if(!error && response.statusCode == 200) {
